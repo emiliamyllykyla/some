51046ce1959dc5615ca4913f8bb8b1e5e93a964c
Emilia <emilia.myllykyla@hotmail.com>
2022-02-15 19:55:02 +0200

    Fix bug: auth state didn't always change to null after logout

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -13,23 +13,30 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [auth, setAuth] = useState(null);
-  const [loadingAuth, setLoadingAuth] = useState(true);
+  const [loadingAuth, setLoadingAuth] = useState(false);
 
   const login = async (data) =>
     loginApi(data).then((res) => res && setAuth(res.user));
-  const logout = () => logoutApi().then(setAuth(null));
+  const logout = () => {
+    setLoadingAuth(true);
+    logoutApi().then(() => {
+      setAuth(null);
+      setLoadingAuth(false);
+    });
+  };
   const register = (data) => registerApi(data);
   const deleteUser = () => deleteUserApi().then(() => setAuth(null));
   const editProfileImage = (name, data) => editProfileImageApi(name, data);
 
   useEffect(() => {
     const authorize = async () => {
+      setLoadingAuth(true);
       authApi().then((res) => {
         res.success ? setAuth(res.user) : logout();
         setLoadingAuth(false);
       });
     };
-    if (!auth) authorize();
+    if (!auth && !loadingAuth) authorize();
   }, [auth]);
 
   return (
